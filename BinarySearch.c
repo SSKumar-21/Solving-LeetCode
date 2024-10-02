0d27b7c76d5a81608cbf51e7fc7889921816d65e
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-10-02 17:24:06 +0530

    Create BinarySearch.c

---
+++ BinarySearch.c
@@ -0,0 +1,19 @@
+int search(int* nums, int numsSize, int target) {
+    int l=0,r=numsSize-1,m;
+    while(l<=r){
+        m=(l+r)/2;
+        if(nums[m]==target){
+            return m;
+            break;
+        }
+        else if(nums[m]>target){
+            r=m-1;
+        }
+        else{
+            l=m+1;
+        }
+    }
+
+     return -1;
+    
+}
