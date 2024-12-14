fe28c084366ed1a9636c245e272aaf0362b3a308
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-14 11:12:46 +0530

    Update 41. First Missing Positive.c

--- 41. First Missing Positive.c
+++ 41. First Missing Positive.c
@@ -34,3 +34,23 @@ int firstMissingPositive(int* nums, int numsSize) {
     free(freq);
     return 100001;
 }
+
+
+/*
+java
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer> set =new HashSet<>();
+        for(int i: nums){
+            if(i>=0){
+                set.add(i);
+            }
+        }
+        int i=1;
+        while(set.contains(i)){
+            i++;
+        }
+        return i;
+    }
+}
+*/
