2e96d72f69258e38db18caaf135b2ec84c0c5147
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-14 11:06:11 +0530

    Update 70 ClimbingStairs.c

--- 70 ClimbingStairs.c
+++ 70 ClimbingStairs.c
@@ -10,3 +10,22 @@ int climbStairs(int n) {
     }
     return ans;
 }
+
+/*
+java
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int x=1,y=1,ans=x+y;
+        for(int i=0;i<n-1;i++){
+            ans=x+y;
+            x=y;
+            y=ans;
+        }
+        return ans;
+}
+}
+*/
