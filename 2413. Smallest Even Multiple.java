76b8045153970aa6ed1a9789afcacd617cb70c1f
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-25 20:05:22 +0530

    Create 2413. Smallest Even Multiple.java

---
+++ 2413. Smallest Even Multiple.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        for(int i=n;;i+=n){
+            if(i%2==0){
+                return i;
+            }
+        }
+    }
+}
+
+/*
+int smallestEvenMultiple(int n) {
+    for(int i=n;;i+=n){
+            if(i%2==0){
+                return i;
+            }
+        }
+}
+*/
