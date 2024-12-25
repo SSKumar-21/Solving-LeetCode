326cd5fb0dec325f66cf42b674fbfe6371d36f03
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-25 19:58:12 +0530

    Create 1952. Three Divisors.java

---
+++ 1952. Three Divisors.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isThree(int n) {
+        int c=1;
+        for(int i=1;i<=(n/2) ; i++){
+            if(n%i==0){
+                c++;
+            }
+        }
+        return c==3;
+    }
+}
+
+/*
+bool isThree(int n) {
+    int c=1;
+        for(int i=1;i<=(n/2) ; i++){
+            if(n%i==0){
+                c++;
+            }
+        }
+        return c==3;
+}
+*/
