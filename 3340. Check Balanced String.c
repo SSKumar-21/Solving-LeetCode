43182c967b0fa74e151fad7b3ebf5a1ae3c14c37
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2024-12-29 11:48:37 +0530

    Update 3340. Check Balanced String.c

--- 3340. Check Balanced String.c
+++ 3340. Check Balanced String.c
@@ -12,3 +12,17 @@ bool isBalanced(char* num) {
     }
     return odd==even;
 }
+/*
+class Solution {
+    public boolean isBalanced(String num) {
+        int s1=0,s2=0;
+        for(int i=0;i<num.length();i+=2){
+            s1+=(num.charAt(i)-'0');
+        }
+        for(int i=1;i<num.length();i+=2){
+            s2+=(num.charAt(i)-'0');
+        }
+        return s1==s2;
+    }
+}
+*/
