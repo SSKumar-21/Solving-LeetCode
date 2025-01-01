afbdac6b81cac8c616c18c30012848fd7cb1b246
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-01 09:11:56 +0530

    Create 2727. Is Object Empty.js

---
+++ 2727. Is Object Empty.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(Array.isArray(obj)){
+        return obj.length == 0;
+    }
+    let arr=Object.keys(obj);
+    return arr.length ==0;
+};
