0da162bc897c679081ad62334a258bba70859967
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-07 12:18:23 +0530

    Update 1431 KidsWiththeGreatestNumberofCandies.c

--- 1431 KidsWiththeGreatestNumberofCandies.c
+++ 1431 KidsWiththeGreatestNumberofCandies.c
@@ -19,3 +19,25 @@ bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* retu
    *returnSize = candiesSize;
    return res;
 }
+
+/*class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> res= new ArrayList<>();
+        int max=candies[0];
+        for(int ele : candies){
+            if(max<ele){
+                max=ele;
+            }
+        }
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies >= max){
+                res.add(true);
+            }
+            else{
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
+*/
