56105d7f71fb36f681ca9636f8092ea077748286
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-07 13:19:11 +0530

    Update 238. Product of Array Except Self.c

--- 238. Product of Array Except Self.c
+++ 238. Product of Array Except Self.c
@@ -19,3 +19,20 @@ int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
     *returnSize=numsSize;
     return res; 
 }
+
+/*
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] arr= new int[nums.length];
+        arr[0]=1;
+        for(int i=1;i<nums.length;i++){
+            arr[i]=arr[i-1]*nums[i-1];
+        }
+        int x=1;
+        for(int i=nums.length-1;i>=0;i--){
+            arr[i]*=x;
+            x*=nums[i];
+        }
+        return arr;
+    }
+}*/
