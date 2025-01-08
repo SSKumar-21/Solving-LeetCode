3ea43d6a138c69532a9c2c0ee07a4d8fa0e6446b
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-08 14:01:14 +0530

    Update 724. Find Pivot Index.c

--- 724. Find Pivot Index.c
+++ 724. Find Pivot Index.c
@@ -13,3 +13,22 @@ int pivotIndex(int* nums, int numsSize) {
     }
     return -1;
 }
+/*
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int l=0,r=0,sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        r=sum-nums[0];
+        for(int i=0;i<nums.length;i++){
+            r=sum-l-nums[i];
+            if(l==r){
+                return i;
+            }
+            l+=nums[i];
+        }
+        return -1;
+    }
+}
+*/
