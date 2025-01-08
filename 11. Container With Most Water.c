d1ad5fe33071727bd83bdfdceaf61d24f145b722
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-08 12:47:57 +0530

    Update 11. Container With Most Water.c

--- 11. Container With Most Water.c
+++ 11. Container With Most Water.c
@@ -16,3 +16,24 @@ int maxArea(int* height, int heightSize) {
     }
     return max;
 }
+/*
+class Solution {
+    public int maxArea(int[] height) {
+      int max=0;
+      int l=0,r=height.length -1;
+      while(l<r){
+        int h= (height[l]>height[r])? height[r] : height[l];
+        int area=(r-l)*h;
+        if(max<area){
+            max=area;
+        }
+        if(height[l]>height[r]){
+            r--;
+        } else{
+            l++;
+        }
+      }  
+      return max;
+    }
+}
+*/
