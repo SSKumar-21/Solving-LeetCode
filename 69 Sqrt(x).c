daea39bc334bfc7431513f60b477b01517bf2867
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-13 10:59:17 +0530

    Update 69 Sqrt(x).c

--- 69 Sqrt(x).c
+++ 69 Sqrt(x).c
@@ -1,3 +1,20 @@
 int mySqrt(int x) {
-    return sqrt(x);
+    long long int i=1;
+    while(i*i<=x){
+        if(i*i==x){
+            return i;
+        }
+        i++;
+    }
+
+    long long int l=1,r=x , m;
+    while(l<=r){
+        m=l+(r-l)/2;
+        if(m*m<x){
+            l=m+1;
+        } else{
+            r=m-1;
+        }
+    }
+    return r;
 }
