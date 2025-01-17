faf4057820817fd29dba405f1d1691d27c867f84
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-17 17:57:31 +0530

    Update 2683. Neighboring Bitwise XOR.java

--- 2683. Neighboring Bitwise XOR.java
+++ 2683. Neighboring Bitwise XOR.java
@@ -7,3 +7,13 @@ class Solution {
         return res==0;
     }
 }
+
+/*
+bool doesValidArrayExist(int* derived, int derivedSize) {
+    int res = derived[0];
+        for(int i=1;i<derivedSize;i++){
+            res = res^derived[i];
+        }
+        return res==0;
+}
+*/
