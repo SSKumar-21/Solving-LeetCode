2e593f689538d7f545940f1621ec5a244cebc24c
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-01-25 11:39:24 +0530

    Update 1267. Count Servers that Communicate.java

--- 1267. Count Servers that Communicate.java
+++ 1267. Count Servers that Communicate.java
@@ -1,3 +1,32 @@
+
+class Solution {
+    public int countServers(int[][] grid) {
+        int[] row = new int[grid.length];
+        int[] col = new int[grid[0].length];
+        Arrays.fill(row,0);
+        Arrays.fill(col,0);
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int res=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1 && Math.max(row[i],col[j])>1){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
+/*
 class Solution {
     public int countServers(int[][] grid) {
         int res = 0;
@@ -29,3 +58,4 @@ class Solution {
         return total;
     }
 }
+*/
