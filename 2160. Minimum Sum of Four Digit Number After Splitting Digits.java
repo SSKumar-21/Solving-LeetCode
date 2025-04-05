a52324c4a0f6bb9b20ba60a22de689fa4fb919b0
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-04-05 12:44:46 +0530

    Create 2160. Minimum Sum of Four Digit Number After Splitting Digits.java

---
+++ 2160. Minimum Sum of Four Digit Number After Splitting Digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        for (int i=0;i<4;i++){
+            arr[i]=num%10;
+            num/=10;
+        }
+        Arrays.sort(arr);
+        int new1 = arr[0]*10 + arr[3];
+        int new2 = arr[1]*10 + arr[2];
+        return new1+new2 ;
+    }
+}
